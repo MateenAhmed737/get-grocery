5169e7e7917e921aac1cc512e75dbb4eaf0765c1
Mateen Ahmed <107365785+MateenAhmed737@users.noreply.github.com>
2023-12-06 11:22:44 +0500

    Login page completed and Slides page

---
+++ src/pages/index.js
@@ -0,0 +1,24 @@
+import Jobs from "./Jobs";
+import Companies from "./Companies";
+import Invoices from "./Invoices";
+import GeneralPage from "./GeneralPage";
+import SlidesManagement from "./SlidesManagement";
+import PaymentMethods from "./PaymentMethods";
+import TransactionLogs from "./TransactionLogs";
+import Dashboard from "./Dashboard";
+import WorkTypes from "./WorkTypes";
+export * from "./Auth";
+export * from "./User";
+export * from "./Settings";
+
+export {
+  Jobs,
+  Companies,
+  GeneralPage,
+  Invoices,
+  SlidesManagement,
+  PaymentMethods,
+  TransactionLogs,
+  Dashboard,
+  WorkTypes,
+};
